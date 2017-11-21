b3373dee5e9e6a6dc99d6839c33065f69f81f08c
Hunaid Hassan <hhunaid@gmail.com>
2017-11-21 23:35:56 +0500

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Hunaid Hassan on 21/11/2017.
+//  Copyright © 2017 Hunaid Hassan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
